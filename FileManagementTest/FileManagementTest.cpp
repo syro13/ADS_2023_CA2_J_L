c3909ddb9816a2e0d4bfab38e757a88d78e48840
syro13 <lowisjakub@gmail.com>
2023-12-03 00:08:36 +0000

    Initial commit
    Initial commit adding FileManagementApp project & FileManagementTest project

---
+++ FileManagementTest/FileManagementTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FileManagementTest
+{
+	TEST_CLASS(FileManagementTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
